62f0171dd71e2eb9a5ee55f6bf8146835b75b252
CodeMarco05 <brandt.marco05@gmail.com>
2025-02-27 14:02:14 +0100

    init with mac

---
+++ mac/.config/nvim/lua/plugins/lsp-config.lua
@@ -0,0 +1,64 @@
+return {
+	{
+		"williamboman/mason.nvim",
+		config = function()
+			require("mason").setup()
+		end,
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		config = function()
+			require("mason-lspconfig").setup({
+				ensure_installed = {
+					"lua_ls",
+					"rust_analyzer",
+					"marksman",
+					"clangd",
+          "jdtls",
+          "pyright"
+				},
+			})
+		end,
+	},
+	{
+		"neovim/nvim-lspconfig",
+		lazy = false,
+		config = function()
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+			local lspconfig = require("lspconfig")
+
+			lspconfig.lua_ls.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.dockerls.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.docker_compose_language_service.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.rust_analyzer.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.clangd.setup({
+				capabilities = capabilities,
+			})
+
+      lspconfig.pyright.setup({
+        capabilities = capabilities
+      })
+
+
+      -- Java
+      lspconfig.jdtls.setup({
+        capabilities = capabilities,
+      })
+
+			-- Key mappings for LSP functions
+			vim.keymap.set("n", "K", vim.lsp.buf.hover, { noremap = true, silent = true })
+			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, { noremap = true, silent = true })
+			vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, { noremap = true, silent = true }) -- Go to Definition
+			vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, { noremap = true, silent = true }) -- Rename
+		end,
+	},
+}
---
+++ mac/.config/nvim/lua/vim-options.lua
@@ -0,0 +1,42 @@
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=2")
+vim.cmd("set softtabstop=2")
+vim.cmd("set shiftwidth=2")
+
+
+-- filetype init for custom options
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "markdown",
+  command = "setlocal tabstop=2 shiftwidth=2 softtabstop=2 expandtab"
+})
+
+
+vim.g.mapleader = " "
+
+vim.opt.clipboard = "unnamedplus"
+
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+vim.api.nvim_set_keymap("n", "<Left>", "<C-w>h", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<Right>", "<C-w>l", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<Up>", "<C-w>k", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<Down>", "<C-w>j", { noremap = true, silent = true })
+
+vim.api.nvim_set_keymap("n", "<Leader>e", "<cmd>lua vim.diagnostic.open_float()<CR>", { noremap = true, silent = true })
+
+vim.opt.scrolloff = 3
+vim.opt.sidescrolloff = 5
+vim.opt.smoothscroll = true
+
+
+-- Slow down mouse scroll
+vim.api.nvim_set_keymap('n', '<ScrollWheelUp>', '2k', { noremap = true })
+vim.api.nvim_set_keymap('n', '<ScrollWheelDown>', '2j', { noremap = true })
+
+
+--vim.diagnostic.config({
+--    virtual_text = false,  -- Disable inline errors
+--   signs = true,          -- Keep signs in the gutter
+--   underline = true,      -- Keep underline for errors
+--})
